549bfe7f8e101611ce94f11c7656f963c4d8b7bf
ictmac4-1 <familiar0224@yahoo.com.tw>
2016-03-17 18:10:34 +0800

    Third

--- timer1.js
+++ timer1.js
@@ -3,7 +3,7 @@ n minus() {
   if (document.getElementById("num")) {
   //document.getElementById():取得頁面中特定id的元素值
     var curr = parseInt(document.getElementById("num").innerHTML);
-    //.innerHTML:取得網頁內的ID元素後，將想要寫入的字串寫入ID元素。
+    //.innerHTML:取得網頁內的ID元素後，將想要寫入的字串寫入ID元素。//
     //parseInt():把字串轉換成整數
   }
 
